0ceb7003b3372f996abe69d9b54085cbc8770826
Nabeel11111 <nabeel.siddiqui@schbang.com>
2024-09-13 19:55:15 +0530

    initial commit

---
+++ app/layout.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './globals.css'
+export const metadata = {
+    title:'Property Pulse',
+    keywords:'rental, property, real estate',
+    description:"find the perfect rental"
+}
+const MainLayout = ({children}) => {
+  return (
+
+    <html lang="en">
+            <body>
+                <main>{children}</main>
+            </body>
+    </html>
+  )
+}
+
+export default MainLayout
